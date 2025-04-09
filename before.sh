9195855f6f65c774ef2182dad2b5a18bd9c01986
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-04-09 22:51:42 +0900

    Delete unused libraries

--- before.sh
+++ before.sh
@@ -14,19 +14,6 @@ if [ ! -d "${HOME}/.vim/bundle/Vundle.vim" ] ; then
   git clone https://github.com/gmarik/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 fi
 
-# container-structure-test
-if [ ! -d "/usr/local/bin/container-structure-test" ] ; then
-  curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-darwin-amd64 && 
-  chmod +x container-structure-test-darwin-amd64 && 
-  mv container-structure-test-darwin-amd64 /usr/local/bin/container-structure-test
-fi
-
-# sdkman
-if [ ! -d "${HOME}/.sdkman" ] ; then
-  curl -s http://get.sdkman.io | zsh
-  source "${HOME}/.sdkman/bin/sdkman-init.sh"
-fi
-
 # symbolic link
 for f in .??* ; do
   [[ "${f}" == ".git" ]] && continue # ignore files
